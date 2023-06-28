8870e2f874ff9b9f9f229d6d958638548586f1e8
Andres Felipe Quiza <132321866+AndresQuiza@users.noreply.github.com>
2023-06-27 22:00:08 -0500

    Add files via upload

---
+++ contact-list-2.js
@@ -0,0 +1,57 @@
+let listaContactos = [
+    (contacto1 = {
+        id:1077840607,
+        nombres: "Andrés Felipe",
+        apellidos: "Quiza Galvis",
+        telefono: 3135541771,
+        ubicaciones: " ",
+        ciudad: "Garzón - Huila",
+        dirección: "carrera 1b 3 bis 29", 
+    }),
+];
+
+function añadirContacto(
+    id,
+    nombres,
+    apellidos,
+    telefono,
+    ubicaciones,
+    ciudad,
+    dirección,
+){
+    return {id, nombres, apellidos, telefono, ubicaciones, ciudad, dirección};
+}
+function eliminarContacto(pos, cantidadContact) {
+    listaContactos.splice(pos, cantidadContact);
+}
+function verLista () {
+    console.log(listaContactos);
+}
+
+
+listaContactos.push(
+    añadirContacto(
+    55066943,
+    "Melva Ximena",
+    "Galvis Arias",
+    3104997159,
+    "",
+    "Garzón - Huila",
+    "Calle 7 # 10 11"
+    )
+);
+
+listaContactos.push(
+    añadirContacto(
+    12202317,
+    "Camilo Andres",
+    "Quiza Zuñiga",
+    3103393317,
+    "",
+    "Garzón - Huila",
+    "Calle 5 este 29 08"
+    )
+);
+verLista();
+eliminarContacto(1, 1);
+verLista();
